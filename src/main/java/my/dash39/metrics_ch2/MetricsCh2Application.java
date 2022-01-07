eca054fe44153543d51504681f41ea6b1833d1f5
Dzianis Haurylavets <dzianis.haurylavets@ab-soft.net>
2022-01-07 18:14:21 +0200

    Copy-paste project from the chapter 1

---
+++ src/main/java/my/dash39/metrics_ch2/MetricsCh2Application.java
@@ -0,0 +1,13 @@
+package my.dash39.metrics_ch2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetricsCh2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetricsCh2Application.class, args);
+    }
+
+}
---
+++ src/test/java/my/dash39/metrics_ch2/MetricsCh2ApplicationTests.java
@@ -0,0 +1,13 @@
+package my.dash39.metrics_ch2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetricsCh2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
